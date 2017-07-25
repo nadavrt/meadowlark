b3ead113454a25067600b51cea3218868407496c
Nadav Rotchild <nadavrt@gmail.com>
2017-07-26 00:19:59 +0800

    Removed the node_modules directory.

--- meadowlark.js
+++ meadowlark.js
@@ -1,16 +1,17 @@
 const express = require('express');
 var app = express();
-
+var fortune = require('./lib/fortune.js');
 //Setup the handlebars view engine
 const handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
-
+app.use(express.static(__dirname + '/public'));
+let fortunes = 
 app.get('/', (req,res) => res.render('home') );
 
-app.get('/about', (req,res) => res.render('about') );
+app.get('/about', (req,res) => { res.render('about', { fortune: fortune.getFortune() }); } );
 
 //Custom 404 function	
 app.use(function(req,res){
