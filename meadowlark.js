d104ec1b05cfa8c10e132438f8eba3021f346506
Nadav Rotchild <nadavr@spotoption.com>
2017-07-27 17:30:01 +0800

    Started learning about Mocha and Chai

--- meadowlark.js
+++ meadowlark.js
@@ -8,11 +8,18 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
-let fortunes = 
+app.use( (req,res,next) => {
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
+
 app.get('/', (req,res) => res.render('home') );
-
-app.get('/about', (req,res) => { res.render('about', { fortune: fortune.getFortune() }); } );
-
+app.get('/about', (req,res) => {
+	res.render('about', { 
+		fortune: fortune.getFortune(), 
+		pageTestScript: '/qa/tests-about.js' 
+	}); 
+});
 //Custom 404 function	
 app.use(function(req,res){
 	res.status('404').render('404');
